fbe2fa88afd1e6c4b6af642128dea3531ca32f83
Kirill <kirillkongrichanichenko@gmail.com>
2025-11-03 16:42:18 -0800

    "worked on math operations in arithmetics"

--- arithmetic_operators/index.js
+++ arithmetic_operators/index.js
@@ -72,4 +72,91 @@ console.log(`index--: ${index}`);
 let result2 = 1 + 2 * 3 ** (2 - 1);
 console.log(`1 + 2 * 3 ** (2 - 1) = ${result2}`);
 
+// Math library functions
+// Math Object Library - built-in object that provides a collection of properties and methods
+
+
+// Math Functions
+
+const PI = Math.PI;
+const E = Math.E;
+
+console.log(PI);
+console.log(E);
+
+x = 3.21;
+y = 2;
+z = x;
+
+// Rounding
+z = Math.round(x);
+console.log(z);
+
+// Floor 
+z = x;
+z = Math.floor(z);
+console.log(z);
+
+// Ceil
+z = x;
+z = Math.ceil(z);
+console.log(z);
+
+// trunc (remove decimal portions)
+z = x;
+z = Math.trunc(z);
+console.log(z);
+
+// Power
+z = Math.pow(x, y);
+console.log(z);
+
+// Square Root
+z = Math.sqrt(z);
+console.log(z);
+
+// Log
+z = Math.log(x);
+console.log(z);
+
+// Cos & Sin & tan
+z = Math.cos(2 * PI);
+console.log(z);
+
+z = Math.sin(PI / 2);
+console.log(z);
+
+z = Math.tan(PI);
+console.log(z);
+
+z = Math.abs(z);
+console.log(z);
+
+// Sign of a number
+console.log(Math.sign(-5)); // Output: -1
+console.log(Math.sign(8));  // Output: 1
+console.log(Math.sign(0));  // Output: 0
+
+// Max/Min
+x = 1;
+y = 2;
+z = 3;
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+console.log(max);
+console.log(min);
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
 
